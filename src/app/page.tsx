2fa6cbc6d673dec97573261c353a9a10690e6024
sarrpan <sarrpan@yahoo.gr>
2025-06-08 07:40:48 +0300

    Fix relative imports to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 // src/app/page.tsx
 
 import React from 'react';
-import AboutSection from '../../components/AboutSection';
-import AdvantagesSection from '../../components/AdvantagesSection';
-import ProductsSection from '../../components/ProductsSection';
+import AboutSection from '../components/AboutSection';
+import AdvantagesSection from '../components/AdvantagesSection';
+import ProductsSection from '../components/ProductsSection';
 import InterestFormSection from '../components/InterestFormSection';
-import LocationSection from '../../components/LocationSection';
+import LocationSection from '../components/LocationSection';
 
 
 export default function HomePage() {
